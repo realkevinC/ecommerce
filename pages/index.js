2cd9cdab33528e365baada8af4792e9e25cc10c9
KC <kevinchen@Kevins-Air.lan>
2023-01-08 19:58:48 -0500

    product display

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
- 
+import {client} from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-const Home = () => {
+const Home = ({ products, bannerData}) => {
   return (
     <>
-    <HeroBanner/>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}/>
 
     <div className='products-heading'>
       <h2>Best Selling products</h2>
@@ -13,13 +13,26 @@ const Home = () => {
     </div>
 
     <div className='products-container'>
-      {['product 1', 'product 2'].map((product)=> product)}
+      {products?.map((product)=> <Product key={product._id} product={product}/>}
     </div>
     
 
-    <FooterBanner/>
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   )
+};
+
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
